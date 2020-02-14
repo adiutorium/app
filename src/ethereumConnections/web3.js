cdf175ff8ff6b158cafb6e1144c2790b7f0522b4
Arvind Kalra <kalarvind97@gmail.com>
2020-02-14 08:01:52 +0530

    get campaiign detail using index created

--- src/ethereumConnections/web3.js
+++ src/ethereumConnections/web3.js
@@ -191,3 +191,26 @@ export const getCampaigns = campaignFoundCallback => {
       .catch(reject)
   })
 }
+
+export const getCampaignDetails = campaignIndex => {
+  return new Promise((resolve, reject) => {
+    callTransaction('getCampaign', campaignIndex)
+      .then(campaignDetail => {
+        const retValObject = {
+          campaignName: campaignDetail.campaignName,
+          ownerAddress: campaignDetail.owner,
+          supportingDocumentsKey: campaignDetail.supportingDocuments,
+          totalDonationSpecific: parseInt(campaignDetail.totalDonations[0], 10),
+          totalDonationOpen: parseInt(campaignDetail.totalDonations[1], 10),
+          donationEndTime: parseInt(campaignDetail.times[0], 10),
+          spendingEndTime: parseInt(campaignDetail.times[1], 10),
+          requiredDonation: parseInt(campaignDetail.requiredDonation, 10),
+          campaignType: campaignDetail.campaignType,
+          donorAddressesOpen: campaignDetail.donorsOpen,
+          donorAddressesSpecific: campaignDetail.donors,
+        }
+        resolve(retValObject)
+      })
+      .catch(reject)
+  })
+}
